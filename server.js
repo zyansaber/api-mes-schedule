242a2df5f7a58612b7b012658236d7fad07ecdf0
zyansaber <zyan112491@gmail.com>
2026-04-20 13:10:26 +1000

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.get("/api/mes-schedule", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("API running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("API running");
 });
