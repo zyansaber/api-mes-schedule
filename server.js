e9d72e82998a2b61dc8981a32a8fc61547bcd75f
zyansaber <zyan112491@gmail.com>
2026-04-29 08:24:21 +1000

    Enable CORS for frontend origins

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const cors = require("cors");
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:4200",
+  "https://snowyapp.com.au"
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+app.options("*", cors());
 
 const BASE_URL = "https://scheduling-dd672-default-rtdb.asia-southeast1.firebasedatabase.app";
 
